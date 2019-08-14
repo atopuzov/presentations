1387731df06b015eab0a66572e4a4e637898aad5
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2019-08-14 23:57:23 +0200

    Add a shell file

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ nixpkgs ? (import ./nix/nixpkgs.nix)
+}:
+let
+  pkgs = import nixpkgs {};
+  revealjs = pkgs.callPackage ./nix/revealjs.nix {};
+
+  mkRevealJs = pkgs.writeShellScriptBin "mkrevealjs" ''
+    ${pkgs.pandoc}/bin/pandoc -t revealjs -V revealjs-url=${revealjs}/reveal.js $*
+  '';
+
+  mkSlides = pkgs.writeShellScriptBin "mkslides" ''
+    ${mkRevealJs}/bin/mkrevealjs --highlight-style=tango -s -o index.html slides.md
+  '';
+in
+  pkgs.stdenv.mkDerivation {
+    name = "presentations-shell";
+    buildInputs = [ pkgs.pandoc revealjs mkRevealJs mkSlides ];
+    shellHook = ''
+      export LANG=en_US.UTF-8
+      echo "Welcome!"
+    '';
+  }
