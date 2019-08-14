320d69963084a3e59a37d57e87a0af8d38db0dd5
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2019-08-14 17:26:47 +0100

    Try out index.html

--- default.nix
+++ default.nix
@@ -5,6 +5,10 @@ let
   revealjs = pkgs.callPackage ./nix/revealjs.nix {};
 in pkgs.linkFarm "presentations" [
   {
+    name = "index.html";
+    path = ./index.html;
+  }
+  {
     name = "calculate-checksum";
     path = pkgs.callPackage ./calculate-checksum/default.nix { inherit revealjs;  };
   }]
