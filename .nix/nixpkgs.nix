2241b6cb468c4dd5104b6c278de2e08463bfec0c
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2020-06-05 13:09:19 +0100

    Bump nixpkgs

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,5 +1,6 @@
+# git ls-remote https://github.com/NixOS/nixpkgs-channels nixos-20.03
 builtins.fetchGit {
   url = "https://github.com/NixOS/nixpkgs-channels";
-  rev = "6d68b920eb2df73d68a9355f9d572dbf97add5f5";
+  rev = "05a32d8e771dc39438835ba0e3141d68ad4e3068";
   ref = "nixos-20.03";
  }
